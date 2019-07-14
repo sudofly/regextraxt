dc511b53ae52c4e10188726bf56538ec4b9422e6
Dean Ayres <deanayres@gmail.com>
2019-07-14 10:12:40 +0100

    updated sorter to inlclude mp

--- sorter.py
+++ sorter.py
@@ -2,6 +2,11 @@ import re
 import csv
 import glob
 import os
+import sys
+import multiprocessing as mp
+import time
+
+
 
 #import the config csv
 
@@ -11,10 +16,11 @@ import os
 #the trash file where unmatched lines go
 trashfile = "trash.txt"
 #the path to the files you want to sort
-inpath = "c:\\Sorter\\Combos\\"
-outpath = "c:\\Sorter\Output\\"
+inpath = "h:\\proc\\Input2\\"
+outpath = "h:\\proc\\Output\\"
 regex = []
 filename = []
+queues = []
 lines_per_file = 10000000
 
 def splitfile(lines,file):
@@ -24,7 +30,7 @@ def splitfile(lines,file):
 			if lineno % lines == 0:
 				if smallfile:
 					smallfile.close()
-				small_filename = inpath + "recycled_trash_{}.txt".format(lineno + lines)
+				small_filename = inpath + "recycled_" + file +"_{}.txt".format(lineno + lines)
 				print (outpath +"Small_file")
 				smallfile = open(small_filename, "w")
 			smallfile.write(line)
@@ -32,53 +38,105 @@ def splitfile(lines,file):
 			smallfile.close()
 	#delete trash
 	os.remove(file)
-		
+	
 def regexfunc(strinput):
-    line_count =0
-    matched = False
-    for regstring in regex:
-        match = re.findall(regstring,strinput,re.IGNORECASE)
-        if match:
-            #print (f'[+] Matched \"{strinput}\" with \"{regstring}\" and saving to ({line_count}) {filename[line_count]}')
-            with open(outpath + filename[line_count],"a") as output:
-                output.write(strinput + "\n")
-            matched = True
-            break
-        line_count += 1
-        
-    if not matched:
-        #print (f"[+] Didnt match {strinput} and saving to trash")
-        with open(trashfile,"a") as output:
-            output.write(strinput + "\n")
-        matched = True
+	line_count =0
+	matched = False
+	#print(f'regexfunc queues {queues}')
+	#print(f'regexfunc filenames {filename}')
+	for regstring in regex:
+		#print (f'trying {strinput} with {regstring}')
+		#print(f'current lines {line_count}')
+		match = re.findall(regstring,strinput,re.IGNORECASE)
+		if match:
+			#print (f'[+] Matched \"{strinput}\" with \"{regstring}\" and saving to ({line_count}) {filename[line_count]} using {queues[line_count]}')
+			#with open(outpath + filename[line_count],"a") as output:
+			#	output.write(strinput + "\n")
+			queues[line_count].put(strinput)
+			#null.put(strinput)
+			matched = True
+			break
+		line_count += 1
+		
+	if not matched:
+		#print (f"[+] Didnt match {strinput} and saving to trash")
+		with open(trashfile,"a") as output:
+			output.write(strinput + "\n")
+		matched = True
 
-#processing the csv file and extracting the regex's
+def listener(msg,filename):
+	'''listens for messages on the q, writes to file. '''
+	f = open(outpath + filename, 'a')
+	while 1:
 
-with open("searches.csv", "r" ) as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    for row in csv_reader:
-        print(f'Read regex {row["regex"]} save to file {row["filename"]}.\n')
-        regex.append(row["regex"])
-        filename.append(row["filename"])
-    print("\n")
+		m = msg.get()
+		# if m == 'kill':
+			# f.write('killed')
+			# break
+		#print ("[+] Writing " + m)
+		sys.stdout.flush()
+		#with open(outpath + filename, 'a') as output:
+		#	output.write(str(m) + "\n")
+			
+		f.write(str(m) + '\n')
+		f.flush()
+	f.close()
+
+def readcsv(searchfile):
+	manager = mp.Manager()
+	pool = mp.Pool(mp.cpu_count() + 2)
+	with open(searchfile, "r" ) as csv_file:
+		csv_reader = csv.DictReader(csv_file, delimiter=',')
+		for row in csv_reader:
+			print(f'Read regex {row["regex"]} save to file {row["filename"]}.')
+			#Append to the global array
+			regex.append(row["regex"])
+			filename.append(row["filename"])
+			#create a queue
+			q = manager.Queue()
+			queues.append(q)
+			
+			#and create a listener for each line file
+			watcher = pool.apply_async(listener, (q,row["filename"]))
+			print (f'[+]setting up {watcher} in the name of {row["filename"]}')
+			print("\n")
+			pool.join()
+	#setup a trash writer
+	
+	print(f'readcsv queues {queues}')
+
+def main():
+	#must use Manager queue here, or will not work
+	#manager = mp.Manager()
+	#q = manager.Queue()  
+	#pool = mp.Pool(mp.cpu_count() + 2)
+	readcsv("searches.csv")
+	time.sleep(3)
+	files = [file for file in glob.glob(inpath + "**/*", recursive=True)]
+	#for file in files:
+		#splitfile(lines_per_file,file)
+		#multithread this in the future
+		
+	for file in files:
+		print("reading " + file)
+		#try:
+		with open(file, 'r') as infile:
+			for line in infile:
+				#feed the line into the regex function
+				regexfunc(line.rstrip())
+			#os.remove(file)
+		#except:
+		#	print("[+]Something went wrong")
+		#	pass
+	#pool.close()
+	#pool.join()
+	splitfile(lines_per_file,trashfile)
 
 
-#look for files in the path variable and open each for reading
 
-files = [file for file in glob.glob(inpath + "**/*", recursive=True)]
-
-
-for file in files:
-    print(file)
-    try:
-        with open(file, 'r') as infile:
-            for line in infile:
-            #feed the line into the regex function
-                regexfunc(line.rstrip())
-            #finished processing so delete the file
-        os.remove(file)
-    except:
-        pass
-
-#cleanup the trash
-splitfile(lines_per_file,trashfile)
+if __name__ == "__main__":
+   start = time.time()
+   main()
+   end = time.time()
+   print('\n')
+   print(f'Total exec time {end - start}')
